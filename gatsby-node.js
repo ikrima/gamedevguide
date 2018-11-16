716f82e434249781488de8e93bdd05314fd5789b
cvluca <z5155740@unsw.edu.au>
2018-11-16 20:35:41 +0000

    add slug

--- gatsby-node.js
+++ gatsby-node.js
@@ -42,3 +42,17 @@ exports.createPages = ({ actions, graphql }) => {
     })
   })
 }
+
+const { createFilePath } = require(`gatsby-source-filesystem`)
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode, basePath: `pages` })
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    })
+  }
+}
