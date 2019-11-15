77309d2faeb611abffd39f0e541af22c4e6658ef
ikrima <github@ikrima.com>
2019-11-15 00:23:06 -0800

    Add back google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -189,6 +189,33 @@ const plugins = [
             icon: siteCfg.favicon,
         },
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          siteCfg.googleAnalyticsID, // Google Analytics / GA
+          // 'AW-CONVERSION_ID', // Google Ads / Adwords / AW
+          // 'DC-FLOODIGHT_ID', // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared accross all trackingIds
+        // gtagConfig: {
+        //   optimize_id: 'OPT_CONTAINER_ID',
+        //   anonymize_ip: true,
+        //   cookie_expires: 0,
+        // },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ['/preview/**', '/do-not-track/me/too/'],
+        },
+      },
+    },
     `gatsby-plugin-react-helmet`,
     {
         resolve: `gatsby-plugin-advanced-sitemap`,
