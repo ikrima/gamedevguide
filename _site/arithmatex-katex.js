94f46357a7c77858c8e2dc36c432d11d63979ed1
ikrima <ikrima@users.noreply.github.com>
2023-03-24 17:16:47 +0000

    Deployed ced4a57a with MkDocs version: 1.4.2

---
+++ _site/arithmatex-katex.js
@@ -0,0 +1,42 @@
+// This is for arithmatex support for katex
+(function () {
+  'use strict';
+
+  var katexMath = (function () {
+      var maths = document.querySelectorAll('.arithmatex'),
+          tex;
+
+      for (var i = 0; i < maths.length; i++) {
+        tex = maths[i].textContent || maths[i].innerText;
+        if (tex.startsWith('\\(') && tex.endsWith('\\)')) {
+          katex.render(tex.slice(2, -2), maths[i], {'displayMode': false});
+        } else if (tex.startsWith('\\[') && tex.endsWith('\\]')) {
+          katex.render(tex.slice(2, -2), maths[i], {'displayMode': true});
+        }
+      }
+  });
+
+  (function () {
+    var onReady = function onReady(fn) {
+      if (document.addEventListener) {
+        document.addEventListener("DOMContentLoaded", fn);
+        // document$.subscribe(fn);
+        // location$.subscribe(fn);
+        // document.addEventListener("DOMContentSwitch", fn);
+      } else {
+        document.attachEvent("onreadystatechange", function () {
+          if (document.readyState === "interactive") {
+            fn();
+          }
+        });
+      }
+    };
+
+    onReady(function () {
+      if (typeof katex !== "undefined") {
+        katexMath();
+      }
+    });
+  })();
+
+}());
---
+++ _site/arithmatex-mathjax.js
@@ -0,0 +1,17 @@
+// This is for arithmatex support for mathjax
+window.MathJax = {
+  tex: {
+    inlineMath: [["\\(", "\\)"]],
+    displayMath: [["\\[", "\\]"]],
+    processEscapes: true,
+    processEnvironments: true
+  },
+  options: {
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex"
+  }
+};
+
+document$.subscribe(() => {
+  MathJax.typesetPromise()
+})
